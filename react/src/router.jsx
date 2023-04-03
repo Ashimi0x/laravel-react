3c72791f86c3556d8760ad7f19e4abfdcaf4ddc9
Abdulwahab Ashimi <princeadebowale3@gmail.com>
2023-04-03 15:53:04 +0100

    Login & Signup

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,21 +1,44 @@
-import {createBrowserRouter} from "react-router-dom";
-import Login from "./views/Login.jsx";
-import Signup from "./views/Signup.jsx";
-import Users from "./views/Users.jsx";
-import NotFound from "./views/NotFound.jsx";
+import {createBrowserRouter, Navigate} from "react-router-dom";
+import Dashboard from "./views/Dashboard";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Login from "./views/Login";
+import NotFound from "./views/NotFound";
+import Signup from "./views/Signup";
+import Users from "./views/Users";
 
 const router = createBrowserRouter( [
     {
-        path: '/login',
-        element: <Login />
+        path: '/',
+        element: <DefaultLayout />,
+        children:[
+            {
+                path: '/',
+                element: <Navigate to="/users" />
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard />
+            },
+            {
+                path: '/users',
+                element: <Users />
+            },
+        ]
     },
     {
-        path: '/signup',
-        element: <Signup />
-    },
-    {
-        path: '/users',
-        element: <Users />
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+        ]
     },
     {
         path: '*',
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom"
+
+
 export default function Signup(){
+
+    const onSubmit = (ev) => {
+        ev.preventDefault()
+    }
+
     return(
-        <div>
-            Signup
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit="{onSubmit}">
+                    <h1 className="title">
+                        Login into your account
+                    </h1>
+                    <input type="name" placeholder="First Name"/>
+                    <input type="name" placeholder="Last Name"/>
+                    <input type="email" placeholder="Email Address"/>
+                    <input type="password" placeholder="Password"/>
+                    <input type="password" placeholder="Password Confirmation"/>
+                    <button className="btn btn-block">Signup</button>
+                    <p className="message">
+                        Already Registered? <Link to="/login">Sign In</Link>
+                    </p>
+                </form>
+            </div>
         </div>
     )
 }
