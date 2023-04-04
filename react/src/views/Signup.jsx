108fd898d7999738e8ad74b8f94f1333d4d60738
Abdulwahab Ashimi <princeadebowale3@gmail.com>
2023-04-04 20:21:05 +0100

    initaiting the api

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,10 +1,39 @@
 import { Link } from "react-router-dom"
-
+import { useRef } from "react"
+import axiosClient from "../axios-client";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function Signup(){
+    const fnameRef = useRef();
+    const lnameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
+
+    const {setUser, setToken} = useStateContext()
+
 
     const onSubmit = (ev) => {
         ev.preventDefault()
+        const payload ={
+            fname: fnameRef.current.value,
+            lname: lanameref.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            passwordConfirmation: passwordConfirmationRef.current.value
+        }
+
+        axiosClient.post('/signup', payload)
+        .then(({data}) => {
+            setUser(data.user)
+            setToken(data.token)
+        })
+        .catch(err => {
+            const response = err.response;
+            if (response && response.status === 422){
+                console.log(response.data.errors);
+            }
+        })
     }
 
     return(
@@ -14,11 +43,11 @@ export default function Signup(){
                     <h1 className="title">
                         Login into your account
                     </h1>
-                    <input type="name" placeholder="First Name"/>
-                    <input type="name" placeholder="Last Name"/>
-                    <input type="email" placeholder="Email Address"/>
-                    <input type="password" placeholder="Password"/>
-                    <input type="password" placeholder="Password Confirmation"/>
+                    <input ref={fnameRef} type="name" placeholder="First Name"/>
+                    <input ref={lnameRef} type="name" placeholder="Last Name"/>
+                    <input ref={emailRef} type="email" placeholder="Email Address"/>
+                    <input ref={passwordRef} type="password" placeholder="Password"/>
+                    <input ref={passwordConfirmationRef} type="password" placeholder="Password Confirmation"/>
                     <button className="btn btn-block">Signup</button>
                     <p className="message">
                         Already Registered? <Link to="/login">Sign In</Link>
